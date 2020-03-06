67bbf45e7f7522c480320784db8fed4b1b36909c
Kossecki <katarina.uzakova@protonmail.com>
2020-03-06 10:17:34 +0000

    calculator with external .js file working

--- src/static/js/calculator.js
+++ src/static/js/calculator.js
@@ -1,4 +1,4 @@
-function fn() {
+function calculator() {
     var ratio = document.getElementById("ratio");
     var chosenRatio = document.getElementById("chosenRatio");
     var displayWeight = document.getElementById("displayWeight");
@@ -7,36 +7,34 @@ function fn() {
     var waterAmountSlider = document.getElementById("sliderValue");
 
     function calculateWeight(water, ratio) {
-      return Math.round(water / ratio);
+        return Math.round(water / ratio);
     }
 
     function calculateWater(water) {
-      return Math.round(water * 0.9);
+        return Math.round(water * 0.9);
     }
 
     function displayAll(e) {
-      chosenRatio.innerHTML = ratio.value;
-      if (e) {
-        if (e.target === waterAmountSlider) {
-          waterAmountInput.value = waterAmountSlider.value;
-        } else if (e.target === waterAmountInput) {
-          waterAmountSlider.value = waterAmountInput.value;
+        chosenRatio.innerHTML = ratio.value;
+        if (e) {
+            if (e.target === waterAmountSlider) {
+                waterAmountInput.value = waterAmountSlider.value;
+            } else if (e.target === waterAmountInput) {
+                waterAmountSlider.value = waterAmountInput.value;
+            }
         }
-      }
-      var waterAmount = parseInt(waterAmountInput.value);
-      displayWeight.innerHTML = calculateWeight(waterAmount, ratio.value);
-      displayYield.innerHTML = calculateWater(waterAmount);
+        var waterAmount = parseInt(waterAmountInput.value);
+        displayWeight.innerHTML = calculateWeight(waterAmount, ratio.value);
+        displayYield.innerHTML = calculateWater(waterAmount);
     }
 
     displayAll();
 
     document.querySelector("form").addEventListener("input", displayAll);
-  }
+}
 
-  function ready(fn) {
-    if (document.readyState != "loading") {
-      fn();
-    } else {
-      document.addEventListener("DOMContentLoaded", fn);
-    }
-  }
+if (document.readyState != "loading") {
+    calculator();
+} else {
+    document.addEventListener("DOMContentLoaded", calculator);
+}
